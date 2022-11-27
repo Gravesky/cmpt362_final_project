73fdac26f5787f2275be9cb01c6401e1fb24fcac
fangk <kaikunf@sfu.ca>
2022-11-27 00:15:59 -0800

    upload the setting layout

--- myChatApp/app/src/main/java/com/example/mychatapp/Setting/SettingFragment.kt
+++ myChatApp/app/src/main/java/com/example/mychatapp/Setting/SettingFragment.kt
@@ -1,25 +1,58 @@
 package com.example.mychatapp.Setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.example.mychatapp.R
 
-class SettingFragment:Fragment() {
-    private lateinit var view1: View
+class SettingFragment: PreferenceFragmentCompat() {
+    private lateinit var to_sfu: Preference
+    private lateinit var to_course: Preference
+    private lateinit var to_github: Preference
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences,rootKey)
 
 
+        to_course = findPreference("TO_COURSE")!!
+        //to_course.summary = "1111111"
+        to_course.setOnPreferenceClickListener {
 
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.action = "android.intent.action.VIEW"
+            intent.data = Uri.parse("https://www.sfu.ca/~xingdong/Teaching/CMPT362/web/cs65.html")
+            startActivity(intent);
 
-        return view
+            true
+        }
+
+        to_sfu = findPreference("TO_SFU")!!
+        to_sfu.setOnPreferenceClickListener {
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.action = "android.intent.action.VIEW"
+            intent.data = Uri.parse("http://www.sfu.ca/computing.html")
+            startActivity(intent);
+
+            true
+        }
+
+        to_github = findPreference("TO_GITHUB")!!
+        to_github.setOnPreferenceClickListener {
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.action = "android.intent.action.VIEW"
+            intent.data = Uri.parse("https://github.com/Gravesky/cmpt362_final_project")
+            startActivity(intent);
+
+            true
+        }
+
     }
 }
