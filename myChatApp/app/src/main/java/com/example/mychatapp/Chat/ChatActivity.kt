f0857451226add90d3f2d1fd3b32c6ef90e2379b
Jialong Qiao <jialongq@sfu.ca>
2022-11-27 17:59:16 -0800

    create chat activity and it's layout

--- myChatApp/app/src/main/java/com/example/mychatapp/Chat/ChatActivity.kt
+++ myChatApp/app/src/main/java/com/example/mychatapp/Chat/ChatActivity.kt
@@ -1,5 +1,13 @@
 package com.example.mychatapp.Chat
 
-class ChatActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mychatapp.R
+
+class ChatActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.chat_activity)
+    }
 
 }
