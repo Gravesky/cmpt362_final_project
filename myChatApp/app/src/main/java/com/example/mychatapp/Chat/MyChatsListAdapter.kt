e9fede047c0e8f85d85b2d7fcab75282f483b051
Zejia Cai <zca99@sfu.ca>
2022-12-03 03:19:52 -0800

    Now the chat history list can show the latest message

--- myChatApp/app/src/main/java/com/example/mychatapp/Chat/MyChatsListAdapter.kt
+++ myChatApp/app/src/main/java/com/example/mychatapp/Chat/MyChatsListAdapter.kt
@@ -7,10 +7,22 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.mychatapp.MainActivity
 import com.example.mychatapp.R
 import com.example.mychatapp.data.SingleChat
+import com.example.mychatapp.data.SingleMsg
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
 
 class MyChatsListAdapter(private val context: Context, private var list: ArrayList<SingleChat>): BaseAdapter() {
+
+    //Firebase variables
+    private lateinit var db: FirebaseDatabase
+
+
     companion object {
         const val TAG = "MyChatsListAdapter:DEBUG:"
     }
@@ -31,6 +43,7 @@ class MyChatsListAdapter(private val context: Context, private var list: ArrayLi
         val view = View.inflate(context, R.layout.chat,null)
         val imageViewPortrait = view.findViewById<ImageView>(R.id.chatImageView)
         val textViewName = view.findViewById<TextView>(R.id.messengerTextView)
+        val latestMessage = view.findViewById<TextView>(R.id.latestMsgTextView)
 
         textViewName.text = list[position].userName
 
@@ -38,6 +51,43 @@ class MyChatsListAdapter(private val context: Context, private var list: ArrayLi
                 1. 用list的groupid访问message的database
                 2. onDataChange message里面相应的groupid
         */
+        db = Firebase.database
+        db.useEmulator("10.0.2.2", 9000)
+
+        val messageRef = db.reference.child(MainActivity.MESSAGES_CHILD).child(list[position].groupId!!)
+
+        val messageListener = object : ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val message = snapshot.getValue<SingleMsg>()
+
+                latestMessage.text = message?.message
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                //TODO: 这个功能是给用户本地删除信息用的：
+                //      1. 找到该message group最后的child
+                //      2. 设置latestMessage
+
+                val message = snapshot.getValue<SingleMsg>()
+
+                latestMessage.text = message?.message
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        messageRef.addChildEventListener(messageListener)
 
         return view
     }
